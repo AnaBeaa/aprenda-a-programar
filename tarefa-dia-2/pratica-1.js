0ac623569e790faf9c0244b8c60bc62a57b0f944
Ana Beatriz <anybeatrizsouza@gmail.com>
2022-08-24 22:28:24 -0300

    console.log

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,8 @@
 let nome = "Ana"
-let email = "ana.gmail.com"
+let email = "ana@gmail.com"
 let profissão = "Dev"
 let idade = 30
 let lugarQueMaisQueroConhecer = "Irlanda"
+
+console.log(`Meu nome é: ${nome}, tenho ${idade}, trabalho como ${profissão}, meu sonho é conhecer a ${lugarQueMaisQueroConhecer} e caso queiram entrar em contato comigo podem me acionar por ${email}`)
+
